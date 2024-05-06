3cce2b2472ab9d2bf0eb529c7da04037932dcee9
LFK <77980399+LucasFKobernic@users.noreply.github.com>
2024-05-06 18:07:06 +0000

    Improved solution of open connection in migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,9 +3,15 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  if (request.method === "GET" || request.method === "POST") {
-    const dbClient = await database.getNewClient();
-
+  const alloweMethods = ["GET", "POST"];
+  if (!alloweMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}"not allowed.`,
+    });
+  }
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
     const defaultMigrationOptions = {
       dbClient: dbClient,
       dryRun: true,
@@ -16,7 +22,7 @@ export default async function migrations(request, response) {
     };
     if (request.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-      await dbClient.end();
+
       return response.status(200).json(pendingMigrations);
     }
     if (request.method === "POST") {
@@ -24,15 +30,16 @@ export default async function migrations(request, response) {
         ...defaultMigrationOptions,
         dryRun: false,
       });
-      await dbClient.end();
+
       if (migratedMigrations.length > 0) {
         return response.status(201).json(migratedMigrations);
       }
       return response.status(200).json(migratedMigrations);
     }
-
-    return response.status(405).end();
-  } else {
-    return response.status(405).end();
+  } catch (e) {
+    console.error(e);
+    throw e;
+  } finally {
+    await dbClient.end();
   }
 }
